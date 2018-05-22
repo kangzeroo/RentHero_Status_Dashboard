bc36cf9ae0a99054d93cfda7f0166ec9489420e4
Kangze Huang <kangzeroo@gmail.com>
2018-05-22 17:10:22 -0400

    nice

--- src/api/API_URLS.js
+++ src/api/API_URLS.js
@@ -4,10 +4,10 @@
 // LOCAL
 
 // DEV
-export const STATUS_MS = 'https://d7c0477d.ngrok.io'
+// export const STATUS_MS = 'https://d7c0477d.ngrok.io'
 
 // PROD
 // export const STATUS_MS = 'https://4564gfh.ngrok.io'
 //
 // // STAGING
-// export const STATUS_MS = 'https://renthero.host:8501'
+export const STATUS_MS = 'https://renthero.host:8501'
